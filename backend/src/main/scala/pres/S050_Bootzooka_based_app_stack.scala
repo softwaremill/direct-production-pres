41e9daa982f87d1fd3901362037fcf119a8b6bb7
adamw <adam@warski.org>
2025-03-19 15:49:59 +0100

    WIP

--- backend/src/main/scala/pres/S050_Bootzooka_based_app_stack.scala
+++ backend/src/main/scala/pres/S050_Bootzooka_based_app_stack.scala
@@ -4,7 +4,7 @@ class S050_Bootzooka_based_app_stack:
 
   val Language = "Scala"
   val DI: Either["MacWire", "Scala"] = Left("MacWire")
-  val HTTP = List("Tapir", "jsoniter")
+  val HTTP = List("Tapir", "Netty", "jsoniter")
   val DB = List("Magnum", "Flyway")
   val Concurrency = "Ox"
   val Observability = List("Logback", "OpenTelemetry")
